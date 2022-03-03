020236ca77ae51ee8b8cfda389b096dbc183033d
MinistrasP <elzkas@gmail.com>
2022-03-03 08:57:48 +0200

    Updates main.h

--- src/main.h
+++ src/main.h
@@ -1,7 +1,7 @@
-int createDatabase(void);
-int insertDataInto(void);
-int printDataOut(void);
-int deleteAllDataDatabase(void);
-void printHelp(void);
-void read_write_DB(void);
-void automaticMode(void);
+int createDatabase(void);        //Creates a database
+int insertDataInto(void);        //Inserts signal data point
+int printDataOut(void);          //Prints out data
+int deleteAllDataDatabase(void); //Deletes all data from database
+void printHelp(void);            //Prints help
+void read_write_DB(void);        //Reads, writes database
+void automaticMode(void);        //Engages automatic mode
